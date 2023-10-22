a42cf3277c75f103dba17d5f513c4f206ddc5ec6
DO Chi <Chi Do@DESKTOP-CPSDNDG.com>
2023-10-22 16:15:32 +0700

    upload

--- detectFire.py
+++ detectFire.py
@@ -3,10 +3,8 @@ import cvzone
 import cv2
 import math
 import time
+import serial
 
-def drawKC(img, x, y, w, h):
-    cv2.line(img, (320, y + int(h / 2)), (x + int(w / 2), y + int(h / 2)), (0, 255, 0), 2)  # line từ Y ra tâm ngọn lửa
-    cv2.line(img, (x + int(w / 2), 240), (x + int(w / 2), y + int(h / 2)), (0, 255, 0), 2)  # line từ X ra tâm ngọn lửa
 def drawKc(img, x,y,w,h):
     cv2.line(img, (320,y+int(h/2)), (x+int(w/2), y+int(h/2)), (0,255,0), 2) # 
     cv2.line(img, (x+int(w/2), 240), (x+int(w/2), y+int(h/2)), (0,255,0), 2) # 
@@ -45,8 +43,8 @@ def handlerAndSendToSignal(image, x,y,w,h):
     return str(goc_quay_Px), str(goc_quay_Py), direction
 
 def drawxy(img):
-    cv2.line(img, (320,0), (320, 480), (0,255,255), 1) # dọc  y
-    cv2.line(img, (0,240), (640, 240), (0,255,255), 1) # ngang x
+    cv2.line(img, (320,0), (320, 480), (221, 222, 217), 1) # dọc  y
+    cv2.line(img, (0,240), (640, 240), (221, 222, 217), 1) # ngang x
     
     cv2.putText(img, "x" , (620 , 230), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 1, cv2.LINE_AA)
     cv2.putText(img, "y" , (330 , 20), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 1, cv2.LINE_AA)
@@ -85,7 +83,7 @@ def YoloDetect(result):
         for box in boxes:
             confidence = box.conf[0]
             confidence = math.ceil(confidence * 100)
-            if confidence > 78:
+            if confidence > 70:
                 print("FIRE")
                 return 1
                
@@ -95,53 +93,51 @@ def YoloDetect(result):
     #return x1,y1,w1,h1
 def main():
     # Running real time from webcam
-    cap = cv2.VideoCapture(0)
+    ser = serial.Serial("COM3", 9600)
+    cap = cv2.VideoCapture(1)
     model = YOLO('nckhUptoGit/nckhDetects/fire.pt')
     fire_cascade = cv2.CascadeClassifier("nckhUptoGit/nckhDetects/fire_detection.xml")
     # Reading the classes
+    img = cap.read()
+    result = model(img,stream=True)
+    x,y,w,h = 0,0,0,0
     
-    
-    #last_send_time = time.time()
+    last_send_time = time.time()
     while True:
         ret,image = cap.read()
         drawxy(image)
         #image = cv2.resize(image,(640,480))
-        # current_time = time.time()
-        # if current_time - last_send_time >= 1.5:  # 0.5 giây = 500ms
-            
-        #     PackageGocPx, PackageGocPy, PackageDirection = redcolor(image)
-        #     str = PackageGocPx + "\n" + PackageGocPy + "," + PackageDirection + "."
-            
-        #     if(PackageDirection != "null" or PackageDirection != "OK"):
-        #         print("hi")
-        #         ser.write(str.encode())
-            
-        #     last_send_time = current_time  
         
-        x,y,w,h = 0,0,0,0
-        fire = fire_cascade.detectMultiScale(image, 1.08, 3)
-       
+            
+        
+        fire = fire_cascade.detectMultiScale(image, 1.05, 3)
         for x, y, w, h in fire:
             print("ok")
             cv2.rectangle(image, (x - 10, y - 10), (x + (w+10), y + (h+10)), (255, 0, 0), 1)
-        
-        if( w != 0):    
+            
+        current_time = time.time()
+        if current_time - last_send_time >= 1.5 and  w != 0:  # 0.5 giây = 500ms
+            
             x1 = x + w + 20
             y1 = y + h + 20
-            image_cut = image[y - 20:y1, x - 20:x1] # v
-            cv2.imshow("goc", image_cut)
-            
+            image_cut = image[y - 20:y1, x - 20:x1] # cắt ảnh
             image_cut = cv2.addWeighted(image_cut, 1, image_cut, 0, -75) #giảm độ sáng
-            cv2.imshow("dark", image_cut)
-            
-            
-            result = model(image_cut,stream=True)
-            if YoloDetect(result) == 1:
-                PackageGocPx, PackageGocPy, PackageDirection = handlerAndSendToSignal(image, x,y,h,w)
-                str = PackageGocPx + "\n" + PackageGocPy + "," + PackageDirection + "."  
-                print(str)
-                
-                 
+            height, width, channels = image_cut.shape
+            if(height > 0 and width > 0):
+                cv2.imshow("cut", image_cut)
+                result = model(image_cut,stream=True)
+                if YoloDetect(result) == 1:
+                    PackageGocPx, PackageGocPy, PackageDirection = handlerAndSendToSignal(image, x,y,h,w)
+                    str = PackageGocPx + "\n" + PackageGocPy + "," + PackageDirection + "."  
+                    if(PackageDirection != "null" or PackageDirection != "OK"):
+                        print("hi", str)
+                        ser.write(str.encode())
+                        
+                        
+            x,y,w,h = 0,0,0,0           
+            last_send_time = current_time  
+                    
+                    
         cv2.imshow('image',image)
         k = cv2.waitKey(1)
         if k%256 == 27:
