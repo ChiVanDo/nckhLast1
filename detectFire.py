36ec3d75900eca67807a81f82c90871e77d6039e
DO Chi <Chi Do@DESKTOP-CPSDNDG.com>
2023-10-22 10:46:45 +0700

    upload22/10

--- detectFire.py
+++ detectFire.py
@@ -45,11 +45,11 @@ def handlerAndSendToSignal(image, x,y,w,h):
     return str(goc_quay_Px), str(goc_quay_Py), direction
 
 def drawxy(img):
-    cv2.line(img, (320,0), (320, 480), (0,255,255), 2) # dọc  y
-    cv2.line(img, (0,240), (640, 240), (0,255,255), 2) # ngang x
+    cv2.line(img, (320,0), (320, 480), (0,255,255), 1) # dọc  y
+    cv2.line(img, (0,240), (640, 240), (0,255,255), 1) # ngang x
     
-    cv2.putText(img, "x" , (620 , 230), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 2, cv2.LINE_AA)
-    cv2.putText(img, "y" , (330 , 20), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 2, cv2.LINE_AA)
+    cv2.putText(img, "x" , (620 , 230), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 1, cv2.LINE_AA)
+    cv2.putText(img, "y" , (330 , 20), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 1, cv2.LINE_AA)
 
 def kcx(x,y,w,h):
     # Tọa độ của điểm 1
@@ -85,7 +85,7 @@ def YoloDetect(result):
         for box in boxes:
             confidence = box.conf[0]
             confidence = math.ceil(confidence * 100)
-            if confidence > 70:
+            if confidence > 78:
                 print("FIRE")
                 return 1
                
@@ -95,12 +95,11 @@ def YoloDetect(result):
     #return x1,y1,w1,h1
 def main():
     # Running real time from webcam
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     model = YOLO('nckhUptoGit/nckhDetects/fire.pt')
     fire_cascade = cv2.CascadeClassifier("nckhUptoGit/nckhDetects/fire_detection.xml")
     # Reading the classes
     
-    check_fire = False
     
     #last_send_time = time.time()
     while True:
@@ -120,44 +119,29 @@ def main():
         #     last_send_time = current_time  
         
         x,y,w,h = 0,0,0,0
-        fire = fire_cascade.detectMultiScale(image, 1.1, 2)
+        fire = fire_cascade.detectMultiScale(image, 1.08, 3)
        
         for x, y, w, h in fire:
             print("ok")
-            cv2.rectangle(image, (x - 10, y - 10), (x - 10 + (w+10), y - 10 + (h+10)), (255, 0, 0), 2)
+            cv2.rectangle(image, (x - 10, y - 10), (x + (w+10), y + (h+10)), (255, 0, 0), 1)
         
         if( w != 0):    
-            x1 = x + w
-            y1 = y + h 
-            image_cut = image[y - 20:y1 + 20 , x - 10:x1 + 20] # v
+            x1 = x + w + 20
+            y1 = y + h + 20
+            image_cut = image[y - 20:y1, x - 20:x1] # v
+            cv2.imshow("goc", image_cut)
             
-            cv2.imshow("he", image_cut)
+            image_cut = cv2.addWeighted(image_cut, 1, image_cut, 0, -75) #giảm độ sáng
+            cv2.imshow("dark", image_cut)
+            
+            
             result = model(image_cut,stream=True)
             if YoloDetect(result) == 1:
                 PackageGocPx, PackageGocPy, PackageDirection = handlerAndSendToSignal(image, x,y,h,w)
                 str = PackageGocPx + "\n" + PackageGocPy + "," + PackageDirection + "."  
                 print(str)
                 
-                
-            # if w != 0:
-            #     drawKc(image, x, y, w, h)
-            #     check_fire = True  
-              
-        # x1,y1,w1,h1 = 0,0,0,0
-        # if(check_fire == True): 
-        #     for x, y, w, h in fire:
-        #         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        #         
-        #     cv2.imshow("1", image_cut)
-        #     result = model(image_cut,stream=True)
-        #     YoloDetect(result)
-            # if YoloDetect(result) == 1:
-            #     PackageGocPx, PackageGocPy, PackageDirection = handlerAndSendToSignal(image, x,y,h,w)
-            #     str = PackageGocPx + "\n" + PackageGocPy + "," + PackageDirection + "."  
-            #     print(str)
-        
-            
-         
+                 
         cv2.imshow('image',image)
         k = cv2.waitKey(1)
         if k%256 == 27:
